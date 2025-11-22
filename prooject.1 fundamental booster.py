3d6789b83fa490a446368abffe410a6b18912354
vishal yadav <yadavvishal59851@gmail.com>
2025-11-22 13:27:03 +0530

    Add files via upload

---
+++ prooject.1 fundamental booster.py
@@ -0,0 +1,19 @@
+print("Welcome to the interactive Personal Data Collector!")
+
+Name = input("please enter your name: ")
+age = input("please emter your age: ")
+height = input("please enter your height in meters : ")
+number = input("please enter your favourite number: ")
+
+print("thank you! Here is the information we collected:")
+
+print("Name: ",(Name),type(Name),id(Name))
+print("age: ",(age),type(age),id(age))
+print("height: ",(height),type(height),id(height))
+print("number: ",(number),type(number),id(number))
+
+birthyear = input("your birth year is approximately: ")
+
+
+print("Thank you for using the personal Data Collector." )
+print("wish you best of luck and good bye")
